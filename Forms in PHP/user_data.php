a1e549f074b5e3ab6b3ea9d22273e54ae3e6280f
Adegbola-GNA <joshuaadenuga78@gmail.com>
2022-10-29 01:34:04 +0100

    week 3 tasks

---
+++ Forms in PHP/user_data.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$birthday = $_POST['birthday'];
+$gender = $_POST['gender'];
+$country = $_POST['country'];
+$data = [$name , $email , $birthday , $gender , $country,"\n"];
+
+$handle = fopen('./userdata.csv', 'a'); //open file in append mode
+fputcsv($handle, $data);
+
+print_r($_POST);
+fclose($handle);
+
+//$filename = './userdata.csv';
+//$file = fopen($filename, 'a');
+//$content = fread($file, filesize($filename)); //reads file
+//echo $content; //printing file data
+
+
+/*
+echo "<br>";
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+echo 'NAME: ' . $name . '<br>';
+echo 'EMAIL: ' . $email . '<br>';
+*/
